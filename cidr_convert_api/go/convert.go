a62da89bf61e47096664427c237758dd2b3e2bd9
albertoefguerrero <75097482+albertoefguerrero@users.noreply.github.com>
2020-12-17 16:44:24 -0600

    Implemented missing functions

--- cidr_convert_api/go/convert.go
+++ cidr_convert_api/go/convert.go
@@ -1,13 +1,53 @@
-package convert
-// These functions need to be implemented
+package main
+import (
+	"net"
+	"strconv"
+)
+
 func cidrToMask(value string) string {
-	return routeGetCidrToMask(value)
+	result := value
+	// If cidr is in range convert
+	cidr, _ := strconv.Atoi(value)
+	if (cidr >= 1 && cidr <= 32) {
+		// Int to mask
+		mask := net.CIDRMask(cidr, 32)
+		// Mask to IP address with proper format
+		result = net.IP(mask).String()
+		return result
+	}
+	result = "Invalid"
+	return result
 }
 
 func maskToCidr(value string) string {
-	return value
+	// if IP is not valid return Invalid
+	valid := ipv4Validation(value)
+	result := value
+
+	if valid == false {
+		result = "Invalid"
+		return result
+	}
+	// Convert mask to CIDR
+	ip := net.ParseIP(value)
+	// IP to 4 byte
+	addr := ip.To4()
+	// 4 byte to Int
+	cidr, _ :=	net.IPv4Mask(addr[0], addr[1], addr[2], addr[3]).Size()
+	// Int to String
+	result = strconv.Itoa(cidr)
+
+	return result
 }
 
 func ipv4Validation(value string) bool {
-	return true
+	ip := net.ParseIP(value)
+    var status bool
+	status = true
+
+	if ip == nil {
+		status = false
+	}
+
+	return status
 }
